e88e2870c7a16048d85c9175bd8948d87933e5ca
Bruno Tesser <99666198+brunotesser@users.noreply.github.com>
2022-03-10 12:29:32 -0300

    Desafio cálculo IMF
    Crie um programa para calcular o IMC e nível de obesidade de uma pessoa.

--- index.js
+++ index.js
@@ -1,25 +1,14 @@
-// DESAFIO 2 CALCULO DE APOSENTADORIA
-// Baseado nos valores acima utilize as fórmulas a seguir para calcular se a pessoa está apta ou não para se aposentar e no fim imprima uma mensagem em tela.
-// O tempo de contribuição mínimo para homens é de 35 anos e, para mulheres, 30 anos;
-// Utilizando a regra 85-95, a soma da idade com o tempo de contribuição do homem precisa ser de no mínimo 95 anos, enquanto a mulher precisa ter no mínimo 85 anos na soma;
+// DESAFIO CALCULO IMC
 
-const nome = 'Bruno'
-const sexo = 'feminino'
-const idade = 21
-const contribuicao = 100
+const nome = 'Juan'
+const peso = 1
+const altura = 1.3
+const sexo = 'transexual'
 
-const condicao = idade + contribuicao
+const imc = (peso / altura) * altura
 
-if (sexo === 'masculino') {
-  if (condicao >= 95 || contribuicao >= 35) {
-    console.log('Você contribuiu igual escravo otario, parabéns')
-  } else {
-    console.log('Você não escravo, seu bosta!')
-  }
-} else if (sexo === 'feminino') {
-  if (condicao >= 85 || contribuicao >= 30) {
-    console.log('Você contribuiu igual escravo otario, parabéns')
-  }
+if (imc >= 30) {
+  console.log('Juan você está gordo pra krl')
 } else {
-  console.log('Você não escravo, seu bosta!')
+  console.log('Juan você ta magrão')
 }
